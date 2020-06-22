5c5dbcc31ffecf98cad477e1ff9def849bda912e
MuteBard <csevere@brandeis.edu>
2020-06-22 05:15:24 -0400

    thonkang

--- src/main/scala/Dao/MongoDBOperations.scala
+++ src/main/scala/Dao/MongoDBOperations.scala
@@ -3,7 +3,6 @@ package Dao
 import com.mongodb.reactivestreams.client.{MongoClient, MongoClients, MongoDatabase}
 
 class MongoDBOperations {
-
 	val uri : String = sys.env.getOrElse("CB_MONGODB_URI", "mongodb://localhost:27017")
 	val dbname : String = sys.env.getOrElse("DBNAME", "crossingbot")
 	val client: MongoClient = MongoClients.create(uri)
