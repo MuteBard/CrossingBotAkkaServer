6dd4ccefce1c681934b5aad539bb10ec2449065a
MuteBard <csevere@brandeis.edu>
2020-06-22 02:50:19 -0400

    adding new mongo uri

--- src/main/scala/Dao/MongoDBOperations.scala
+++ src/main/scala/Dao/MongoDBOperations.scala
@@ -3,7 +3,12 @@ package Dao
 import com.mongodb.reactivestreams.client.MongoClients
 
 class MongoDBOperations {
-	val client = MongoClients.create("mongodb://localhost:27017")
+
+	val prod_uri = "mongodb://heroku_lk06xxv0:1s78gsn37rdg05sk60metdsakq@ds155288.mlab.com:55288/heroku_lk06xxv0"
+	val local_uri = "mongodb://localhost:27017"
+	val client = MongoClients.create(prod_uri)
 	protected val db = client.getDatabase("crossingbot")
 	client
 }
+
+
